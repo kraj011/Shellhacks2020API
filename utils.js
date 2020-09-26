812de3c3147695905ac97e563268af596508e236
David Krajewski <davidkrajewski@yahoo.com>
2020-09-26 11:46:34 -0400

    Updated to use env file for all api keys

--- utils.js
+++ utils.js
@@ -1,71 +1,69 @@
 const Twit = require("twit");
 var T = new Twit({
-    consumer_key: "",
-    consumer_secret: "",
-    access_token: "",
-    access_token_secret: "",
-    timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
-    strictSSL: true, // optional - requires SSL certificates to be valid.
+	consumer_key: process.env.CONSUMER_KEY,
+	consumer_secret: process.env.CONSUMER_SECRET,
+	access_token: process.env.ACCESS_TOKEN,
+	access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+	timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
+	strictSSL: true, // optional - requires SSL certificates to be valid.
 });
 
-
 const sendDM = (userId, message) => {
-    return new Promise((resolve, reject) => {
-        const params = {
-            event: {
-                type: "message_create",
-                message_create: {
-                    target: {
-                        recipient_id: userId,
-                    },
-                    message_data: {
-                        text: message,
-                    },
-                },
-            },
-        };
-        T.post("direct_messages/events/new", params)
-            .then((result) => {
-                resolve();
-            })
-            .catch((err) => {
-                reject(err)
-            });
-    })
-}
+	return new Promise((resolve, reject) => {
+		const params = {
+			event: {
+				type: "message_create",
+				message_create: {
+					target: {
+						recipient_id: userId,
+					},
+					message_data: {
+						text: message,
+					},
+				},
+			},
+		};
+		T.post("direct_messages/events/new", params)
+			.then((result) => {
+				resolve();
+			})
+			.catch((err) => {
+				reject(err);
+			});
+	});
+};
 
 const getLatestTweet = (screenName) => {
-    return new Promise((resolve, reject) => {
-        T.post("statuses/user_timeline", {
-                screen_name: screenName,
-                count: 1
-            })
-            .then((result) => {
-                resolve(result);
-            })
-            .catch((err) => {
-                reject(err)
-            });
-    })
-}
+	return new Promise((resolve, reject) => {
+		T.post("statuses/user_timeline", {
+			screen_name: screenName,
+			count: 1,
+		})
+			.then((result) => {
+				resolve(result);
+			})
+			.catch((err) => {
+				reject(err);
+			});
+	});
+};
 
 const getUserInfo = (screenName) => {
-    return new Promise((resolve, reject) => {
-        T.post("users/lookup", {
-                screen_name: screenName
-            })
-            .then((result) => {
-                resolve(result.data)
-            })
-            .catch((err) => {
-                reject(err)
-            });
-    })
-
-}
+	return new Promise((resolve, reject) => {
+		T.post("users/lookup", {
+			screen_name: screenName,
+		})
+			.then((result) => {
+				resolve(result.data);
+			})
+			.catch((err) => {
+				reject(err);
+			});
+	});
+};
 
 module.exports = {
-    sendDM,
-    getLatestTweet,
-    getUserInfo
-}
+	sendDM,
+	getLatestTweet,
+	getUserInfo,
+};
